0d6826c04caf151af84e9882a9341014a61cf540
girvel <widauka@ya.ru>
2020-01-28 16:20:03 +0500

    Realize transcription

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ostem
+
+Ostem is a simple artificial language that I have created for Dying Space game. This is a library to work with this language.
--- ostem/__init__.py
+++ ostem/__init__.py
@@ -1,2 +1,17 @@
+import json
+
+global_alphabet = {
+    "'": "ʲ"
+}
+
+with open('../assets/alphabets/classic.json', encoding='UTF-8') as f:
+    alphabet = {**json.load(f), **global_alphabet}
+
+
+def transcribe(word, alphabet):
+    return (alphabet.get(word[0], word[0]) + transcribe(word[1:], alphabet)) if word else word
+
+
 if __name__ == '__main__':
-    print('Hello, world!')
+    while True:
+        print(transcribe(input(), alphabet))
